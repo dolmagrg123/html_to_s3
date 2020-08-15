da9a700417c0d3b82fe6068a4e5a790b3c040ec6
dolmagrg1231 <dolmagurung75@gmail.com>
2020-08-15 15:12:00 -0400

    Saved the html file

--- main.py
+++ main.py
@@ -1,23 +1,46 @@
+# import boto3
+
 class HtmlDocument:
     #that lets you initialize some HTML for a new document.
-    def __init__(self,name):
-        self.name = name
+    def __init__(self,content):
+        self.content = content
 
 class HtmlMananger:
     #that defines functions that let you create a new HTML document, 
     # and save the document to your files.
     # write-html.py
-    def create_html():
-        f = open('dolma.html','w')
-        message = """<html>
-        <head></head>
-        <body><p>Hello World!</p></body>
-        </html>"""
 
-        f.write(message)
+    def __init__(self):
+        self.document = None
+
+    def create_html(self):
+        
+        message = """<html>
+        <head>This is a new html file</head>
+        <h1> My name is Dolma </h1>
+        <body>
+        <p> My name is Dolma Gurung. I am learning AWS</p>
+        <p> This file will be uploaded to Amazon S3 </p>
+        </body>
+        </html>"""
+        newdoc = HtmlDocument(message)
+        self.document = newdoc
+        print(newdoc)
+
+        
+    def save_html_file(self):
+        f = open('dolma.html','w')
+        f.write(self.document.content)
         f.close()
 
-    create_html()
 
-# class AWSManager:
-#     #defines the connections to boto3 and some functions that let you save your file to S3.
+class AWSmanager:
+    pass
+#   s3 = boto3.client('s3')
+#   #define connections to boto3 and save file to s3
+#   def save_to_s3():   
+#     boto3.client('s3').upload_file('helloworld.html', 'lmtd-class', 'heyclass.html')
+
+manager = HtmlMananger()
+manager.create_html()
+manager.save_html_file()
