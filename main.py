e1cdbc968ff195399ccb4997c5aa8b1130aa0622
dolmagrg1231 <dolmagurung75@gmail.com>
2020-08-15 12:15:57 -0400

    Creating classes

---
+++ main.py
@@ -0,0 +1,11 @@
+class HtmlDocument:
+    #that lets you initialize some HTML for a new document.
+    def __init__(self,name):
+        self.name=name
+
+class HtmlMananger:
+    #that defines functions that let you create a new HTML document, 
+    # and save the document to your files.
+
+class AWSManager:
+    #defines the connections to boto3 and some functions that let you save your file to S3.
