f28718848f6c28dc65d2c0ab3e29fe1a5d499c48
dolmagrg1231 <dolmagurung75@gmail.com>
2020-08-15 12:53:42 -0400

    Created a new html file

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 class HtmlDocument:
     #that lets you initialize some HTML for a new document.
     def __init__(self,name):
-        self.name=name
+        self.name = name
 
 class HtmlMananger:
     #that defines functions that let you create a new HTML document, 
     # and save the document to your files.
+    # write-html.py
+    def create_html():
+        f = open('dolma.html','w')
+        message = """<html>
+        <head></head>
+        <body><p>Hello World!</p></body>
+        </html>"""
 
-class AWSManager:
-    #defines the connections to boto3 and some functions that let you save your file to S3.
+        f.write(message)
+        f.close()
+
+    create_html()
+
+# class AWSManager:
+#     #defines the connections to boto3 and some functions that let you save your file to S3.
