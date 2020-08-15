f8e579fefe138e50f2529550f01e1ead532a1d1a
dolmagrg1231 <dolmagurung75@gmail.com>
2020-08-15 16:29:07 -0400

    Project Completed

--- main.py
+++ main.py
@@ -36,12 +36,18 @@ class HtmlMananger:
 
 class AWSmanager:
     def __init__(self):
-        pass
+        self.s3 = boto3.resource('s3')
 
 #   #define connections to boto3 and save file to s3
     def save_to_s3(self):  
         s3 = boto3.client('s3') 
         boto3.client('s3').upload_file('hello.html', 'lmtd-class', 'dolma.html')
+
+    def listBucketFile(self, bucketName):
+        bucket = self.s3.Bucket(bucketName)
+        files = bucket.objects.all()
+        for file in files:
+            print(file.key)
 
 manager = HtmlMananger()
 manager.create_html()
@@ -49,3 +55,4 @@ manager.save_html_file()
 
 aws = AWSmanager()
 aws.save_to_s3()
+aws.listBucketFile("lmtd-class")
